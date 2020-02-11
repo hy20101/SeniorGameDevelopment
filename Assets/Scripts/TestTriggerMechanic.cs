71ddfcb6784aaf6af3083b1023e09765527512a9
PisitShare <pisit.share@outlook.com>
2020-02-11 15:35:51 +0700

    Add Trigger Test Mechanic Script

---
+++ Assets/Scripts/TestTriggerMechanic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTriggerMechanic : MonoBehaviour
+{
+    //When the Primitive exits the collision, it will change Color
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("กรีีดๆ someone entered " + other.name);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // Destroy everything that leaves the trigger
+        Destroy(other.gameObject);
+    }
+}
