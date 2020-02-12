99cba0c7af6386cbd1dc79858d622b607b9c6f7a
Phattharathorn Dararattanaroj <pan_pattaratorn@hotmail.com>
2020-02-12 17:09:43 +0700

    Add TestScene/TestCore_EnemySpawnPoint Finish camera zoom in/out and enemy spawn point mechanic

---
+++ Assets/Scripts/Core/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] enemies;
+    public int maxEnemies;
+    public int currentEnemies;
+    int randomSpawnPoint;
+    int randomEnemies;
+
+    void Start()
+    {
+        currentEnemies = 0;
+        InvokeRepeating("SpawnEnemies", 0f, 1f);
+    }
+
+    void SpawnEnemies()
+    {
+        if (currentEnemies < maxEnemies)
+        {
+            randomSpawnPoint = Random.Range(0, spawnPoints.Length);
+            randomEnemies = Random.Range(0, enemies.Length);
+            Instantiate(enemies[randomEnemies], spawnPoints[randomSpawnPoint].position, Quaternion.identity);
+            currentEnemies++;
+        }
+    }
+    
+}
