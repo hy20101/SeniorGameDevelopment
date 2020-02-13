12f673182f8a9ef26d81d5e8a62a41046536862f
Ruttanan Wongsrisakul <hy.20101@hotmail.com>
2020-02-13 14:56:52 +0700

    Add Test Screen and change controller to Rigidbody

--- Assets/Scripts/Core/Controller.cs
+++ Assets/Scripts/Core/Controller.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     public float rotationRate = 360;
-    public float moveSpeed = 2;
+    public float moveSpeed = 5;
+    public float jumpSpeed = 5;
+
+    public Rigidbody rb;
     
     private string moveInputAxis = "Vertical";
     private string turnInputAxis = "Horizontal";
+
+    void start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -17,6 +25,11 @@ public class Controller : MonoBehaviour
         float turnAxis = Input.GetAxis(turnInputAxis);
 
         ApplyInput(moveAxis, turnAxis);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump(100);
+        }
     }
 
     private void ApplyInput(float moveAxis, float turnAxis)
@@ -27,11 +40,17 @@ public class Controller : MonoBehaviour
 
     private void Move(float input)
     {
-        transform.Translate(Vector3.forward * input * moveSpeed * Time.deltaTime);
+        rb.AddForce(transform.forward * input * moveSpeed, ForceMode.Force);
+        //transform.Translate(Vector3.forward * input * moveSpeed * Time.deltaTime);
     }
 
     private void Turn(float input)
     {
         transform.Rotate(0, input * rotationRate * Time.deltaTime, 0);
+    }
+
+    private void Jump(float input)
+    {
+        rb.AddForce(Vector3.up * jumpSpeed * Time.deltaTime * input, ForceMode.Impulse);
     }
 }
