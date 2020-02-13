8627aad364059d4aa887d371b879a2237bd04885
Phattharathorn Dararattanaroj <pan_pattaratorn@hotmail.com>
2020-02-13 11:23:34 +0700

    Update CameraController script (Add zoom in/out mechanic)

--- Assets/Scripts/Core/CameraController.cs
+++ Assets/Scripts/Core/CameraController.cs
@@ -4,25 +4,36 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField]
-    private Transform targetToFollow;
-    [SerializeField]
-    private Vector3 targetOffset;
-    [SerializeField]
-    private float movementSpeed;
+    public Transform targetToFollow;
+    public Vector3 targetOffset;
+    public float movementSpeed;
+    public float minZoom;
+    public float maxZoom;
+    public float sensitivity;
 
     void Start()
     {
-        
+        targetOffset.y = 20f;
+        movementSpeed = 3f;
+        minZoom = 10f;
+        maxZoom = 20f;
+        sensitivity = 5f;
     }
 
     void Update()
     {
         MoveCamera();
+        ZoomInOut();
     }
 
     void MoveCamera()
     {
         transform.position = Vector3.Lerp(transform.position, targetToFollow.position + targetOffset, movementSpeed * Time.deltaTime);
+    }
+
+    void ZoomInOut()
+    {
+        targetOffset.y += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        targetOffset.y = Mathf.Clamp(targetOffset.y, minZoom, maxZoom);
     }
 }
