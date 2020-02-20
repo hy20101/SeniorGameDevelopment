68959fd38150b727eaf285744bab6a423b877f5c
Ruttanan Wongsrisakul <hy.20101@hotmail.com>
2020-02-20 16:22:10 +0700

    Add Main menu and Screen Loader with loading bar

---
+++ Assets/Scripts/Core/SceneTransitionMouse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionMouse : MonoBehaviour
+{
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void LoadPreScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
