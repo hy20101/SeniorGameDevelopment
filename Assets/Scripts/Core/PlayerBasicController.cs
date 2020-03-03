d84f16b79d3bc523415f0cf874c39d80d0fd8d65
Phattharathorn Dararattanaroj <pan_pattaratorn@hotmail.com>
2020-03-03 14:30:24 +0700

    Add Idle and Walk animations and Player Prefab

---
+++ Assets/Scripts/Core/PlayerBasicController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBasicController : MonoBehaviour
+{
+    public Animator animator;
+    public float rotationRate = 200;
+    public float moveSpeed = 80;
+    public float jumpSpeed = 10;
+
+    public Rigidbody rb;
+    
+    private string moveInputAxis = "Vertical";
+    private string turnInputAxis = "Horizontal";
+
+    //for jumping
+    private Collider col;
+    private LayerMask groundLayer;
+
+    void start()
+    {
+        rb = GetComponent<Rigidbody>();//for movement
+        col = GetComponent<Collider>();//for jumping
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveAxis = Input.GetAxis(moveInputAxis);
+        float turnAxis = Input.GetAxis(turnInputAxis);
+
+        ApplyInput(moveAxis, turnAxis);
+        //|| Input.GetKeyDown("joystick button 0")
+        /*if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump(100);
+        }*/
+    }
+
+    private void ApplyInput(float moveAxis, float turnAxis)
+    {
+        Move(moveAxis);
+        Turn(turnAxis);
+    }
+
+    private void Move(float input)
+    {
+        Vector3 moveForce = transform.forward * input * moveSpeed;
+        if (animator != null)
+        {
+            animator.SetFloat("speed", moveForce.magnitude);
+        }      
+        rb.AddForce(moveForce, ForceMode.Force);
+        //transform.Translate(Vector3.forward * input * moveSpeed * Time.deltaTime);
+    }
+
+    private void Turn(float input)
+    {
+        transform.Rotate(0, input * rotationRate * Time.deltaTime, 0);
+    }
+
+    private void Jump(float input)
+    {
+        rb.AddForce(Vector3.up * jumpSpeed * Time.deltaTime * input, ForceMode.Impulse);
+    }
+    //TODO: Fix isGrounded
+    /*private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position,Vector3.down, col.bounds.extents.y + 0.1f);
+    }*/
+}
