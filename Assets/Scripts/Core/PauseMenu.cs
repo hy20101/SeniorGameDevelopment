a8b1c48db2a752824533decb0f0cbfbb691752e1
Ruttanan Wongsrisakul <hy.20101@hotmail.com>
2020-03-30 19:59:07 +0700

    adding Pause Menu

---
+++ Assets/Scripts/Core/PauseMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool isPause = false;
+    public GameObject PauseUI;
+
+    void Start()
+    {
+        HidePauseMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            if (isPause == false)
+            {
+                Pause();
+            }
+            else if (isPause == true)
+            {
+                Resume();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        isPause = true;
+        ShowPauseMenu();
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        isPause = false;
+        HidePauseMenu();
+    }
+
+    private void ShowPauseMenu()
+    {
+        PauseUI.SetActive(true);
+    }
+
+    private void HidePauseMenu()
+    {
+        PauseUI.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
