29425948e634764aa56995b783782133901701f1
tickytickk <52659229+tickytickk@users.noreply.github.com>
2020-04-22 14:39:33 +0700

    Add files via upload

---
+++ Assets/WalkController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WalkController : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            anim.SetInteger("direction", 1);
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            anim.SetInteger("direction", 0);
+        }
+    }
+}
