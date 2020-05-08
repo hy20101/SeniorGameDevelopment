a2965066408779e4234d078ce8c60d34d111d8ba
Phattharathorn Dararattanaroj <pan_pattaratorn@hotmail.com>
2020-05-08 21:20:08 +0700

    Update, Fix problem in prefabs and map

--- Assets/Scripts/Core/Unit.cs
+++ Assets/Scripts/Core/Unit.cs
@@ -29,7 +29,7 @@ public class Unit : MonoBehaviour
     public bool armorUpgrade2;
 
     [SerializeField] CharacterClass characterClass;
-    public CharacterSelectionControl charaSelect;
+    //public CharacterSelectionControl charaSelect;
 
     private void Start()
     {
