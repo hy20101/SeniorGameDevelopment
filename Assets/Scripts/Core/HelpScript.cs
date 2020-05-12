ae311706323a19301fb299d9fbdf4781624636c1
Ruttanan Wongsrisakul <hy.20101@hotmail.com>
2020-05-12 14:05:27 +0700

    Add Killcount, ScoreSystem to Scene1. Add prefab for score, killcount, scoremanager

---
+++ Assets/Scripts/Core/HelpScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpScript : MonoBehaviour
+{
+    public GameObject HelpUI;
+    public GameObject AboutUs;
+
+    void Start()
+    {
+        HelpUI.SetActive(false);
+        AboutUs.SetActive(false);
+    }
+
+    public void OpenHelp()
+    {
+        HelpUI.SetActive(true);
+    }
+
+    public void CloseHelp()
+    {
+        HelpUI.SetActive(false);
+    }
+
+    public void OpenUS()
+    {
+        AboutUs.SetActive(true);
+    }
+
+    public void CloseUS()
+    {
+        AboutUs.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Core/ScoreSystem/TestScoreSystem/KillCountManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class KillCountManager : MonoBehaviour
+{
+    public static KillCountManager instance;
+
+    [SerializeField]
+    TextMeshProUGUI KillCounterText;
+    [HideInInspector]
+
+    public int KillCounted;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if(KillCounted == 3)
+        {
+            loadScene();
+        }
+    }
+
+    public void UpdateKillCounterUI()
+    {
+        KillCounterText.text = KillCounted.ToString();
+        print("ha hoi kill");
+        //PlayerPrefs.SetInt("score", ScorePoint);
+    }
+
+    public void loadScene()
+    {
+        SceneManager.LoadScene(7);
+    }
+}
