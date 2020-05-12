8e5e9492cab50cc8cf091e5a97ee7e3fb2f43e80
Phattharathorn Dararattanaroj <pan_pattaratorn@hotmail.com>
2020-05-12 14:48:26 +0700

    Update UI for potion Complete

---
+++ Assets/Scripts/Core/PotionUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PotionUI : MonoBehaviour
+{
+    Text t;
+    HealingButton potion;
+
+    private void Start()
+    {
+        t = GetComponent<Text>();
+        potion = GameObject.FindGameObjectWithTag("Player").GetComponent<HealingButton>();
+    }
+
+    private void Update()
+    {
+        t.text = potion.potionCount.ToString();
+    }
+}
